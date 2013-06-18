eac52cdeb4305003d11eaf8d020c00a08b54b462
benfreeman <benfreeman@gmail.com>
2013-06-17 23:41:43 -0700

    some more fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :require_login, except: [:new, :create]
+  before_filter :signed_in_user,  only:  [:edit, :update]
+  before_filter :correct_user,    only:  [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -37,7 +38,15 @@ class UsersController < ApplicationController
 
   private
 
-    def require_login
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
